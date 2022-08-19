aedb8ef806c7e564ba1f27b418b6f976ccef9999
zejian <110577803+kongzejian@users.noreply.github.com>
2022-08-19 10:47:59 +0800

    Update disc02.py

--- discussion/disc02.py
+++ discussion/disc02.py
@@ -1,3 +1,14 @@
+def keep_ints(cond, n):
+    for i in range(1,n+1):
+        if(cond(i)):
+            print(i)
+            
+def make_keeper(n):
+    def f(cond):
+        for i in range(1,n+1):
+            if(cond(i)):
+                print(i)
+    return f
 
 
 def print_delayed(x):
@@ -5,3 +16,12 @@ def print_delayed(x):
         print(x)
         return print_delayed(y)
     return p
+
+def print_n(n):
+    def inner_print(x):
+            if(n==0):
+                print("done")
+            else:
+                print(x)
+            return print_n(n-1)
+    return inner_print
